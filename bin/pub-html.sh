63918d385da663e3860dbe8be77ac30bc9a236ae
John Henry Thompson <jht1493@gmail.com>
2022-07-01 00:46:16 -0400

    external/media

--- bin/pub-html.sh
+++ bin/pub-html.sh
@@ -6,7 +6,7 @@ cd ${0%/*}
 excludes="--exclude .DS_Store --exclude .git --exclude node_modules --exclude external/media"
 delete=--delete
 test=
-test=--dry-run
+# test=--dry-run
 verbose=
 verbose=v
 
@@ -18,18 +18,18 @@ homepage=p5VideoKit/demo
 rpath="${siteroot}/${homepage}"
 rdest=$host:${rpath}
 
+# Create directory for upload
 ssh $host mkdir -p $rpath
 
-# Remove server uploads directory, establish symbolic link later
-# ssh $host rm -rf $rpath/uploads
+# Remove server directory external/media, establish symbolic link later
+ssh $host rm -rf $rpath/external/media
 
-# source=../p5-projects
 source=../src
 # echo $verbose $delete $test
 echo -razO$verbose $excludes $delete $test
 echo "rsync from $source"
 echo "        to $rdest"
-# rsync -razO$verbose $excludes $delete $test "$source/" "$rdest/"
+rsync -razO$verbose $excludes $delete $test "$source/" "$rdest/"
 
 # Symbolic link external/media to large media files folder
 ssh $host ln -s /home/bitnami/htdocs/a1/skt/assets $rpath/external/media
