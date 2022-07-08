d8045bacd7140503f613590f52fa4a4cbc24f38b
John Henry Thompson <jht1493@gmail.com>
2022-07-08 16:42:46 -0400

    Update sketch.js

--- src/sketch.js
+++ src/sketch.js
@@ -1,23 +1,25 @@
-// let my_canvas;
 let videoKit;
 
 p5.disableFriendlyErrors = true; // disables FES to improve performance
 
 function setup() {
+  // Report startup time for debugging
   let lapse = window.performance.now() - a_start_now;
   console.log('setup lapse', lapse);
 
   // pixelDensity(1); // does not appear to affect live media
-  // !!@ retire my_canvas
-  // my_canvas = createCanvas(100, 100);
+
   createCanvas(100, 100);
 
   // p5VideoKit create must follow createCanvas
 
+  // effects for import, will appear at top of patch effect menu
   let effects = [
     { label: 'example', import_path: 'import/eff_example' },
     { label: 'ncell', import_path: 'import/eff_ncell' },
   ];
+
+  // settings for import, will appear at top of settings menu
   let settings = [
     { label: 'slant-4.json', import_path: 'settings/demo/slant-4.json' },
     { label: '2x2.json', import_path: 'settings/demo/2x2.json' },
