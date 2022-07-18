7fc0561a1c427df86f90742dabe259bea48ab614
John Henry Thompson <jht1493@gmail.com>
2022-07-18 19:42:11 -0400

    137

--- src/videoKit/let/a_settingMetas.js
+++ src/videoKit/let/a_settingMetas.js
@@ -2,6 +2,7 @@
 export let a_settingMetas = [
 { label: 'live1', import_path: 'settings/baked/live1.json' },
 { label: 'live2', import_path: 'settings/baked/live2.json' },
+{ label: 'live4-corner-posenet', import_path: 'settings/baked/live4-corner-posenet.json' },
 { label: 'live4-lobby', import_path: 'settings/baked/live4-lobby.json' },
 { label: 'live4', import_path: 'settings/baked/live4.json' },
 { label: 'ticker', import_path: 'settings/baked/ticker.json' },
