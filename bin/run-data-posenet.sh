6e2f7e3dc96f21c4a1b247acb5e98725c3caca55
John Henry Thompson <78864877+jht9629@users.noreply.github.com>
2023-02-15 23:08:11 -0500

    Update run-data-posenet.sh

--- bin/run-data-posenet.sh
+++ bin/run-data-posenet.sh
@@ -4,4 +4,5 @@ cd ${0%/*}
 cd ../desktop
 
 # npm run start -- --full --restart_time 23:59:59 --screen 1 --s 0-color-line-1900
-npm run start -- --full --restart_period 6:0:0 --screen 1 --d settings/2021/1-data-posenet.json 
+# npm run start -- --full --restart_period 6:0:0 --screen 1 --d settings/2021/1-data-posenet.json 
+npm run start -- --restart_period 6:0:0 --screen 1 --d settings/2021/1-data-posenet.json --ddebug
