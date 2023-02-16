f8f78d2fe86cc347d02abec9a3515aad1e355eed
John Henry Thompson <jht1493@gmail.com>
2023-02-16 11:28:36 -0500

    run-skin

--- bin/run-skin-left.sh
+++ bin/run-skin-left.sh
@@ -3,4 +3,5 @@ cd ${0%/*}
 
 cd ../desktop
 
-npm run start -- --full --restart_time 23:59:59 --screen 2 --d settings/2021/0-skin-tone-left.json
+# npm run start -- --full --restart_time 23:59:59 --screen 2 --d settings/2021/0-skin-tone-left.json
+npm run start -- --restart_time 23:59:59 --screen 1 --d settings/2021/0-skin-tone-left.json --ddebug
--- bin/run-skin-right.sh
+++ bin/run-skin-right.sh
@@ -3,4 +3,5 @@ cd ${0%/*}
 
 cd ../desktop
 
-npm run start -- --full --restart_time 23:59:59 --screen 1 --d settings/2021/0-skin-tone-right.json
+# npm run start -- --full --restart_time 23:59:59 --screen 1 --d settings/2021/0-skin-tone-right.json
+npm run start -- --restart_time 23:59:59 --screen 1 --d settings/2021/0-skin-tone-right.json --ddebug
