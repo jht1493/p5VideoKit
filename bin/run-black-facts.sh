0d24439fb1fd16aa9b6cf5f8070fb4d4d6b4f899
John Henry Thompson <jht1493@gmail.com>
2023-02-16 12:24:26 -0500

    Update run-black-facts.sh

--- bin/run-black-facts.sh
+++ bin/run-black-facts.sh
@@ -6,6 +6,6 @@ cd ../desktop
 # npm run start -- --full --restart_time 23:59:59 --screen 1 --s 0-color-line-1900
 # npm run start -- --ddebug --restart_period 1:00 --screen 1 --root https://www.blackfacts.com
 # npm run start -- --full --screen 1 --restart_period 23:59:59 --root https://www.blackfacts.com
-npm run start -- --full --screen 2 --restart_period 3:00 --root https://www.blackfacts.com
+npm run start -- --full --screen 1 --restart_period 3:00 --root "https://blackfacts.com/kiosk?delay=2000&playlist=today"
  
 
