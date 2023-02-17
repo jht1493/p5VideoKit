9a1cf44eecbae83e121eeeec3904339e455eae5e
John Henry Thompson <jht1493@gmail.com>
2023-02-17 00:06:20 -0500

    Update run-dano-faces.sh

--- bin/run-dano-faces.sh
+++ bin/run-dano-faces.sh
@@ -7,6 +7,7 @@ cd ../desktop
 # npm run start -- --ddebug --restart_period 1:00 --screen 1 --root https://www.blackfacts.com
 # npm run start -- --full --screen 1 --restart_period 23:59:59 --root https://www.blackfacts.com
 # npm run start -- --ddebug --screen 2 --restart_period 5:00 --root https://www.hypercinema.org/OSC/Installation/Installation.html  
-npm run start -- --full --screen 2 --restart_period 7:00 --root ../zoned/dano-face/Installation.html  
+#npm run start -- --full --screen 1 --restart_period 7:00 --root ../zoned/dano-face/Installation.html  
+npm run start -- --full --screen 1 --root ../zoned/dano-face/Installation.html  
 
 
