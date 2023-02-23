551642176bd2fd55532ef67d09a82ae4277cf3c0
John Henry Thompson <jht1493@gmail.com>
2023-02-23 09:28:04 -0500

    run-black-debate-web

---
+++ bin/run-black-debate-web.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+cd ${0%/*}
+
+cd ../desktop
+
+# npm run start -- --full --restart_time 23:59:59 --screen 1 --s 0-color-line-1900
+# npm run start -- --ddebug --restart_period 1:00 --screen 1 --root https://www.blackfacts.com
+# npm run start -- --full --screen 1 --restart_period 23:59:59 --root https://www.blackfacts.com
+npm run start -- --full --screen 1 --restart_time 01:00:00 --root "https://blackfacts.com/kiosk?delay=2000&playlist=EBFh8Elfjj4"
+ 
+
+# https://www.youtube.com/watch?v=EBFh8Elfjj4
+# How W.E.B. DuBois Set the Terms of Debate About Race in 20th-Century America (2003)
+
+# https://www.youtube.com/watch?v=xIGJd4nP_f4
+# WEB DuBois: A Biography In Four Voices by Louis Massiah
---
+++ bin/run-black-scribe-web.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+cd ${0%/*}
+
+cd ../desktop
+
+# npm run start -- --full --restart_time 23:59:59 --screen 1 --s 0-color-line-1900
+# npm run start -- --ddebug --restart_period 1:00 --screen 1 --root https://www.blackfacts.com
+# npm run start -- --full --screen 1 --restart_period 23:59:59 --root https://www.blackfacts.com
+npm run start -- --full --screen 1 --restart_time 01:00:00 --root "https://blackfacts.com/kiosk?delay=2000&playlist=xIGJd4nP_f4"
+ 
+
+# https://www.youtube.com/watch?v=xIGJd4nP_f4
+# WEB DuBois: A Biography In Four Voices by Louis Massiah
