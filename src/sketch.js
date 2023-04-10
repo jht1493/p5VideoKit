09265aee434f22450492c555eb61cff0bc0a25fb
John Henry Thompson <jht1493@gmail.com>
2023-04-10 08:41:30 -0400

    eff_a_example_props

--- src/sketch.js
+++ src/sketch.js
@@ -22,10 +22,10 @@ function setup() {
 
   // effects for import, will appear at top of patch effect menu
   let effects = [
+    { label: 'a_example_props', import_path: 'effects/eff_a_example_props.js', ui_label: 'a_example_props' },
     { label: 'live_gallery', import_path: 'effects/eff_live_gallery.js', ui_label: 'live_gallery' },
     { label: 'movie_grid', import_path: 'effects/eff_movie_grid.js', ui_label: 'movie_grid' },
     { label: 'ncell', import_path: 'effects/eff_ncell.js', ui_label: 'ncell' },
-    { label: 'props_example', import_path: 'effects/eff_props_example.js', ui_label: 'props_example' },
     { label: 'shader_clamp', import_path: 'effects/eff_shader_clamp.js', ui_label: 'shader_clamp' },
     { label: 'shader_ripple', import_path: 'effects/eff_shader_ripple.js', ui_label: 'shader_ripple' },
     { label: 'skin_tone_main', import_path: 'effects/eff_skin_tone_main.js', ui_label: 'skin_tone_main' },
