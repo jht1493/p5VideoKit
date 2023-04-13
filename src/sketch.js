d10d7f668c80c1ac58b7a04e873c04d747204790
John Henry Thompson <jht1493@gmail.com>
2023-04-13 09:04:42 -0400

    Update sketch.js
    enable movie_grid

--- src/sketch.js
+++ src/sketch.js
@@ -20,7 +20,7 @@ function setup() {
 
   // must call createCanvas before new p5VideoKit
 
-  // effects for import, will appear at top of patch effect menu
+  // effects for import, will appear at top of the effect menu
   let effects = [
     { label: 'a_example_props', import_path: 'effects/eff_a_example_props.js', ui_label: 'a_example_props' },
     { label: 'a_slit_scan', import_path: 'effects/eff_a_slit_scan.js', ui_label: 'a_slit_scan' },
@@ -29,7 +29,7 @@ function setup() {
     { label: 'shader_ripple', import_path: 'effects/eff_shader_ripple.js', ui_label: 'shader_ripple' },
     { label: 'skin_tone_main', import_path: 'effects/eff_skin_tone_main.js', ui_label: 'skin_tone_main' },
     { label: 'live_gallery', import_path: 'effects/eff_live_gallery.js', ui_label: 'live_gallery' },
-    // { label: 'movie_grid', import_path: 'effects/eff_movie_grid.js', ui_label: 'movie_grid' },
+    { label: 'movie_grid', import_path: 'effects/eff_movie_grid.js', ui_label: 'movie_grid' },
   ];
 
   // settings for import, will appear at top of settings menu
