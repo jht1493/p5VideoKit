a576b79d835c2916ea3ce4bf774d18e89042ecd9
John Henry Thompson <jht1493@gmail.com>
2023-04-15 04:09:53 -0400

    maze_spin
    video_color
    do_random=0 bounce to sequential mazes
    do_random=1 cycle sequential to random maze
    do_random=2 to bounce between random maze

--- src/effects/maze_spin/MazeSpin.js
+++ src/effects/maze_spin/MazeSpin.js
@@ -29,7 +29,9 @@ export default class MazeSpin {
     this.d = int(this.width / this.ncells);
 
     this.output = createGraphics(this.width, this.height);
-
+    if (this.video_color) {
+      this.input_copy = createImage(this.width, this.height);
+    }
     this.output.noFill();
     this.output.strokeWeight(this.d * this.strokeWeight);
 
@@ -45,7 +47,14 @@ export default class MazeSpin {
 
     this.timer = new SecondsTimer();
     this.timer.setPeriod(this.step_period);
-    this.draw_step = 'draw_maze_step';
+    if (this.do_random == 2) {
+      array_random(this.next);
+      this.draw_step = 'draw_maze_random2';
+    } else if (this.do_random == 1) {
+      this.draw_step = 'draw_maze_random1';
+    } else {
+      this.draw_step = 'draw_maze_step0';
+    }
 
     report_1ofn(this);
     div_report(this, this.target, 'setup');
@@ -53,10 +62,165 @@ export default class MazeSpin {
 
   prepareOutput() {
     // console.log('MazeSpin prepareOutput');
-
+    if (this.video_color) {
+      // copy(srcImage, sx, sy, sw, sh, dx, dy, dw, dh)
+      this.input_copy.copy(this.input, 0, 0, this.width, this.height, 0, 0, this.width, this.height);
+    }
     this[this.draw_step]();
   }
 
+  draw_maze_progress() {
+    this.draw_maze_at(this.timer.progress());
+  }
+
+  draw_maze_still() {
+    this.draw_maze_at(1.0);
+  }
+
+  draw_maze_at(at) {
+    // this.output.background(220);
+    this.output.clear();
+    let tangle = HALF_PI * at;
+    let half = this.d / 2;
+    for (let index = 0; index < this.pts.length; index++) {
+      let [x, y] = this.pts[index];
+      let now = this.now[index];
+      let target = this.target[index];
+      let angle = now == target ? 0 : tangle;
+      if (this.video_color) {
+        let col = this.input_copy.get(x, y);
+        this.output.stroke(col);
+      }
+      if (now) {
+        this.drawLeft(x, y, this.d, half, angle);
+      } else {
+        this.drawRight(x, y, this.d, half, angle);
+      }
+    }
+  }
+
+  // sequential
+  draw_maze_step0() {
+    this.draw_maze_progress();
+    if (this.timer.arrived()) {
+      this.timer.setPeriod(this.pause_period);
+      this.draw_step = 'draw_maze_pause0';
+    }
+  }
+
+  draw_maze_pause0() {
+    this.draw_maze_still();
+    if (this.timer.arrived()) {
+      array_add(this.now, this.delta);
+      array_add(this.next, this.delta);
+      //   div_report(this, this.target, 'draw_maze_step0');
+      this.timer.setPeriod(this.step_period);
+      this.draw_step = 'draw_maze_step0';
+    }
+  }
+
+  // random to random
+  draw_maze_random2() {
+    this.draw_maze_progress();
+    if (this.timer.arrived()) {
+      this.timer.setPeriod(this.pause_period);
+      this.draw_step = 'draw_maze_pause2';
+    }
+  }
+
+  draw_maze_pause2() {
+    this.draw_maze_still();
+    if (this.timer.arrived()) {
+      this.timer.setPeriod(this.step_period);
+      let oldNow = this.now;
+      this.now = this.next;
+      this.next = oldNow;
+      this.target = this.next;
+      array_random(this.target);
+      this.draw_step = 'draw_maze_random2';
+    }
+  }
+
+  // --
+  // sequencail - random - sequential
+  draw_maze_random1() {
+    this.draw_maze_progress();
+    if (this.timer.arrived()) {
+      this.timer.setPeriod(this.pause_period);
+      this.draw_step = 'draw_maze_pause1';
+    }
+  }
+
+  draw_maze_pause1() {
+    this.draw_maze_still();
+    if (this.timer.arrived()) {
+      array_add(this.now, this.delta);
+      array_add(this.next, this.delta);
+      array_random(this.random);
+      this.target = this.random;
+      div_report(this, this.target, 'draw_maze_pause1');
+      this.timer.setPeriod(this.step_period);
+      this.draw_step = 'draw_maze_random1_step1';
+    }
+  }
+
+  draw_maze_random1_step1() {
+    this.draw_maze_progress();
+    if (this.timer.arrived()) {
+      this.timer.setPeriod(this.pause_period);
+      this.draw_step = 'draw_maze_random1_pause1';
+    }
+  }
+
+  draw_maze_random1_pause1() {
+    this.draw_maze_still();
+    if (this.timer.arrived()) {
+      let now_save = this.now;
+      this.now = this.target;
+      this.target = now_save;
+      div_report(this, this.target, 'draw_maze_random1_pause1');
+      this.timer.setPeriod(this.step_period);
+      this.draw_step = 'draw_maze_random1_step2';
+    }
+  }
+
+  draw_maze_random1_step2() {
+    this.draw_maze_progress();
+    if (this.timer.arrived()) {
+      this.timer.setPeriod(this.pause_period);
+      this.draw_step = 'draw_maze_random1_pause2';
+    }
+  }
+
+  draw_maze_random1_pause2() {
+    this.draw_maze_still();
+    if (this.timer.arrived()) {
+      this.now = this.target;
+      this.target = this.next;
+      div_report(this, this.target, 'draw_maze_random1_pause2');
+      this.timer.setPeriod(this.step_period);
+      this.draw_step = 'draw_maze_random1';
+    }
+  }
+
+  // --
+  drawLeft(x, y, len, half, angle) {
+    this.output.push();
+    this.output.translate(x + half, y + half);
+    this.output.rotate(angle);
+    this.output.line(-half + 0, -half + 0, -half + len, -half + len);
+    this.output.pop();
+  }
+
+  drawRight(x, y, len, half, angle) {
+    this.output.push();
+    this.output.translate(x + half, y + half);
+    this.output.rotate(angle);
+    this.output.line(-half + len, -half + 0, -half + 0, -half + len);
+    this.output.pop();
+  }
+
+  // --
   make_spiral_pts() {
     this.pts = new SpiralWalker(this).points();
     return this.pts.length;
@@ -85,122 +249,5 @@ export default class MazeSpin {
 
     this.pts = pts;
     return n;
-  }
-
-  draw_maze() {
-    // this.output.background(220);
-    this.output.clear();
-    let tangle = HALF_PI * this.timer.progress();
-    let half = this.d / 2;
-    for (let index = 0; index < this.pts.length; index++) {
-      let [x, y] = this.pts[index];
-      let now = this.now[index];
-      let target = this.target[index];
-      let angle = now == target ? 0 : tangle;
-      if (now) {
-        this.drawLeft(x, y, this.d, half, angle);
-      } else {
-        this.drawRight(x, y, this.d, half, angle);
-      }
-    }
-  }
-
-  draw_maze_step() {
-    this.draw_maze();
-
-    if (this.timer.arrived()) {
-      array_add(this.now, this.delta);
-      array_add(this.next, this.delta);
-
-      if (!this.do_random) {
-        div_report(this, this.target, 'draw_maze_step');
-      }
-
-      this.timer.setPeriod(this.pause_period);
-
-      this.draw_step = 'draw_maze_pause';
-    }
-  }
-
-  draw_maze_pause() {
-    if (this.timer.arrived()) {
-      this.timer.setPeriod(this.step_period);
-
-      this.draw_step = this.do_random ? 'draw_maze_random' : 'draw_maze_step';
-    }
-  }
-
-  draw_maze_random() {
-    array_random(this.random);
-    this.target = this.random;
-
-    div_report(this, this.target, 'draw_maze_random');
-
-    this.timer.setPeriod(this.step_period);
-
-    this.draw_step = 'draw_maze_random_step';
-  }
-
-  draw_maze_random_step() {
-    this.draw_maze();
-
-    if (this.timer.arrived()) {
-      let now_save = this.now;
-      this.now = this.target;
-      this.target = now_save;
-
-      // div_report(this, this.target, 'draw_maze_random_step');
-
-      this.timer.setPeriod(this.pause_period);
-
-      this.draw_step = 'draw_maze_random_pause';
-    }
-  }
-
-  draw_maze_random_pause() {
-    if (this.timer.arrived()) {
-      this.timer.setPeriod(this.step_period);
-
-      this.draw_step = 'draw_maze_random_pause_step';
-    }
-  }
-
-  draw_maze_random_pause_step() {
-    this.draw_maze();
-
-    if (this.timer.arrived()) {
-      this.now = this.target;
-      this.target = this.next;
-
-      div_report(this, this.target, 'draw_maze_random_pause_step');
-
-      this.timer.setPeriod(this.pause_period);
-
-      this.draw_step = 'draw_maze_random_pause2';
-    }
-  }
-
-  draw_maze_random_pause2() {
-    if (this.timer.arrived()) {
-      this.timer.setPeriod(this.step_period);
-
-      this.draw_step = 'draw_maze_step';
-    }
-  }
-
-  drawLeft(x, y, len, half, angle) {
-    this.output.push();
-    this.output.translate(x + half, y + half);
-    this.output.rotate(angle);
-    this.output.line(-half + 0, -half + 0, -half + len, -half + len);
-    this.output.pop();
-  }
-
-  drawRight(x, y, len, half, angle) {
-    this.output.push();
-    this.output.translate(x + half, y + half);
-    this.output.rotate(angle);
-    this.output.line(-half + len, -half + 0, -half + 0, -half + len);
-    this.output.pop();
   }
 }
--- src/effects/maze_spin/eff_maze_spin.js
+++ src/effects/maze_spin/eff_maze_spin.js
@@ -5,15 +5,19 @@ import MazeSpin from './MazeSpin.js?v={{vers}}';
 export default class eff_maze_spin {
   static meta_props = [
     // { prop: 'num_prop', label: 'prop1', selection: [0, 1] },
-    { prop: 'ncells', selection: [9, 13, 15] },
+    { prop: 'ncells', selection: [9, 13, 15, 31, 63] },
     { prop: 'strokeWeight', selection: [0.5, 0.25, 0.33, 0.66, 0.75] },
     { prop: 'delta', selection: [1, -1] },
     { prop: 'do_spiral', selection: [1, 0] },
-    { prop: 'do_random', selection: [1, 0] },
-    { prop: 'do_report', selection: [0, 1] },
+    { prop: 'do_random', selection: [1, 0, 2] },
+    // do_random=0 bounce to sequential mazes
+    // do_random=1 cycle sequential to random maze
+    // do_random=2 to bounce between random maze
+    { prop: 'do_report', selection: [0, 1, 2, 3, 4, 6, 8] },
     {},
     { prop: 'step_period', selection: [1.0, 0, 0.25, 0.5, 2.0, 3.0, 4.0] },
     { prop: 'pause_period', selection: [1.0, 0, 0.25, 0.5, 2.0, 3.0, 4.0] },
+    { prop: 'video_color', selection: [0, 1] },
   ];
 
   constructor(props) {
@@ -23,6 +27,7 @@ export default class eff_maze_spin {
     // console.log('eff_maze_spin meta_props', eff_maze_spin.meta_props);
 
     let my = {
+      input: this.input,
       width: this.input.width,
       height: this.input.height,
       // ncells: this.ncells,
@@ -37,6 +42,7 @@ export default class eff_maze_spin {
     for (const ent of eff_maze_spin.meta_props) {
       my[ent.prop] = this[ent.prop];
     }
+    console.log('eff_maze_spin my', my);
 
     this.mazeSpin = new MazeSpin(my);
 
