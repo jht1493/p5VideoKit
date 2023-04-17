6dad510543e28cf41cf5436fb53dbbd6a61cc161
John Henry Thompson <jht1493@gmail.com>
2023-04-17 13:31:02 -0400

    305 dice.dapi

--- src/videoKit/p5VideoKit.js
+++ src/videoKit/p5VideoKit.js
@@ -22,6 +22,11 @@ class p5VideoKit {
     }
     this.init(config).then(() => {
       console.log('videoKit.init done');
+
+      // Report startup lapse time
+      let init_lapse = window.performance.now - dice.startTime;
+      dice.dapi('init_done', { init_lapse });
+      //
     });
   }
 
@@ -44,3 +49,40 @@ class p5VideoKit {
     console.log('p5VideoKit draw stub');
   }
 }
+
+var dice = {};
+
+dice.dapi = function (arg, arg2, result) {
+  if (dice.debug) console.log('dice arg=' + arg + ' arg2=' + JSON.stringify(arg2));
+  var opt = arg;
+  if (typeof arg2 != 'undefined') {
+    opt = {};
+    opt[arg] = arg2;
+  }
+  if (typeof result == 'string') {
+    opt._result_str = result;
+  } else if (typeof result == 'function') {
+    var rtag = dice.result_rtag + '';
+    opt._result_rtag = rtag;
+    dice.result_rtag++;
+    dice.result_funcs[rtag] = result;
+  }
+  if (window.webkit && window.webkit.messageHandlers && window.webkit.messageHandlers.dice) {
+    window.webkit.messageHandlers.dice.postMessage(opt);
+  } else {
+    if (dice.warning) console.log('dice opt=' + JSON.stringify(opt));
+  }
+};
+dice.result_funcs = {};
+dice.result_rtag = 1;
+dice.result_rvalue = function (rtag, value) {
+  var func = dice.result_funcs[rtag];
+  if (func) {
+    delete dice.result_funcs[rtag];
+    func(value);
+  } else {
+    console.log('dice.result_rvalue missing rtag=' + rtag);
+  }
+};
+
+dice.startTime = window.performance.now();
